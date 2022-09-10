7f1bb0a6aa326e83bdc9bc2f07689aaba5768c91
Aaron Galuzzi <agaluzzi@IN-SITU.COM>
2022-09-10 15:38:50 -0600

    Add MAUI template (dotnet new maui)

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace guitar_theory;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace guitar_theory;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
